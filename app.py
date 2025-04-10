64c4b7ec0e84ba374e661560e879963f3c1b119b
Dmitriy <DreadDima12345@yandex.ru>
2025-04-10 23:16:19 +0300

    Final

--- app.py
+++ app.py
@@ -15,15 +15,39 @@ def store2():
         data = json.load(file)
     return jsonify(data)
 
-@app.route('/5ka_cat')
+@app.route('/okey')
 def store3():
+    with open('all_products_okey.json') as file:
+        data = json.load(file)
+    return jsonify(data)
+
+@app.route('/perekrestok')
+def store4():
+    with open('all_products_perekrestok.json') as file:
+        data = json.load(file)
+    return jsonify(data)
+
+@app.route('/5ka_cat')
+def store5():
     with open('categories_5ka.json') as file:
         data = json.load(file)
     return jsonify(data)
 
 @app.route('/magnit_cat')
-def store4():
+def store6():
     with open('categories_magnit.json') as file:
+        data = json.load(file)
+    return jsonify(data)
+
+@app.route('/okey_cat')
+def store7():
+    with open('categories_okey.json') as file:
+        data = json.load(file)
+    return jsonify(data)
+
+@app.route('/perekrestok_cat')
+def store8():
+    with open('categories_perekrestok.json') as file:
         data = json.load(file)
     return jsonify(data)
 
